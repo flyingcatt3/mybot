38b0821b935b7bf80cbafd7219b439d5814b3601
flyingcatt3 <jones184189@gmail.com>
2020-12-06 21:22:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ async def send():
             s=s3+'\n'+s4
             await channel.send(s)
         await asyncio.sleep(300)
+        await bot.connect()
 bot.loop.create_task(send())       
 bot.run("NzgyMzA1NTA1ODQyMDM2ODA2.X8KQxw.zLwqJ4OjksO5NcEEIOBYYGbl5_4")
 
