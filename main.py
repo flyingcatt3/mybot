e324e7716deba3f1416248e937aeaeb0e6c03201
flyingcatt3 <jones184189@gmail.com>
2020-12-13 21:19:22 +0800

    1.1

--- main.py
+++ main.py
@@ -2,46 +2,60 @@ import asyncio,logging
 from discord.ext import commands
 from facebook_scraper import get_posts
 
-def scraper(sort,target):
-    return ([post[sort] for post in get_posts(target, pages=1)][-1])
+def scraper(sort,target,N):
+    return ([post[sort] for post in get_posts(target, pages=1)][N])
 bot=commands.Bot(command_prefix='/')
 async def scrape():
-    #channel=bot.get_channel(701153967412871268)
-    i=j=oldurl_LEA=oldurl_gw=0
+    
+    #define
+    i=j=oldurl_LEA=oldurl_gw=oldurl_qmo=0
+
     await bot.wait_until_ready()
     while 1:
-        #s13=text,s24=url
-        newurl_LEA=scraper('post_url','LearningEnglishAmericanWay')
+        #s135=text,s246=url,s7=images
+        newurl_LEA=scraper('post_url','LearningEnglishAmericanWay',-1)
         if newurl_LEA!=oldurl_LEA:
-            s1=scraper('text','LearningEnglishAmericanWay')
+            s1=scraper('text','LearningEnglishAmericanWay',-1)
             oldurl_LEA=s2=newurl_LEA
         else:
             s1=s2='0'
 
-        newurl_gw=scraper('post_url','gainwind')
+        newurl_gw=scraper('post_url','gainwind',-1)
         if newurl_gw!=oldurl_gw:
-            s3=scraper('text','gainwind')
+            s3=scraper('text','gainwind',-1)
             oldurl_gw=s4=newurl_gw
         else:
             s3=s4='0'
+
+        newurl_qmo=scraper('post_url','qmoleenglish',0)
+        if newurl_qmo!=oldurl_qmo:
+            s5=scraper('text','qmoleenglish',0)
+            s7=scraper('images','qmoleenglish',0)
+            s7='\n'.join(s7)
+            oldurl_qmo=s6=newurl_qmo
+        else:
+            s5=s6=s7='0'
+
         i+=1
-        if s1!=s2 or s3!=s4:
+
+        if s1!=s2 and s3!=s4:
             s=s1+'\n'+s2+'\n'+"------"+'\n'+s3+'\n'+s4
-            #await channel.send('debug-all-'+str(i))
         elif s1!=s2:
             s=s1+'\n'+s2
-            #await channel.send('debug-LEA-'+str(i))
         elif s3!=s4:
             s=s3+'\n'+s4
-            #await channel.send('debug-GW'+str(i))
         else:
             s=0
-        if s!=0:
+        if s!=0 or s5!=0:
             j+=1
             logging.info(j)
-            await bot.get_channel(701153967412871268).send(s+'\n'+str(j))
-        #else:
-            #await channel.send(content='debug-NONE-'+str(i),delete_after=1)
+            if s!=0:
+                await bot.get_channel(701153967412871268).send(s+'\n')
+            if s5!=0:
+                await bot.get_channel(701153967412871268).send(s5+s6+'\n')
+                if s7!=0:
+                    await bot.get_channel(701153967412871268).send(s7+'\n')
+            await bot.get_channel(701153967412871268).send(str(j))
         await asyncio.sleep(300)
 logging.basicConfig(level=logging.INFO)
 bot.loop.create_task(scrape())
--- test.py
+++ test.py
@@ -1,11 +1,7 @@
 import time
 from facebook_scraper import get_posts
 def scraper(sort,target):
-    print([post[sort] for post in get_posts(target, pages=1)][-1])
-while True:
-    scraper('post_url','gainwind')
-    time.sleep(5)
-#scraper('text','LearningEnglishAmericanWay')
-#scraper('post_url','LearningEnglishAmericanWay')
-#scraper('text','gainwind')
-#scraper('post_url','gainwind')
+    s=[post[sort] for post in get_posts(target, pages=1)][-1]
+    s='\n'.join(s)
+    print(s)
+scraper('images','gainwind')
