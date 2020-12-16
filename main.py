4d98f5b304b5dad9461c0001e2f772b3a062b8c2
flyingcatt3 <jones184189@gmail.com>
2020-12-17 05:23:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ channel1=701153967412871268
 intents = discord.Intents(messages=True, guilds=True, members=True)
 discord.MemberCacheFlags(online=True)
 start = time.time()
-bot.loop.create_task(scrape())
-logging.basicConfig(level=logging.INFO)
-bot.run(token)
+
 
 def scraper(sort,target,N):
     return ([post[sort] for post in get_posts(target, pages=1,timeout=10)][N])
@@ -26,54 +24,54 @@ async def scrape():
     while 1:
         try:
             newurl_LEA=scraper('post_url','LearningEnglishAmericanWay',-1)
+        
+            #s135=text,s246=url,s7=images
+            if newurl_LEA!=oldurl_LEA:
+                s1=scraper('text','LearningEnglishAmericanWay',-1)
+                oldurl_LEA=s2=newurl_LEA
+            else:
+                s1=s2='0'
+
+            newurl_gw=scraper('post_url','gainwind',-1)
+            if newurl_gw!=oldurl_gw:
+                s3=scraper('text','gainwind',-1)
+                oldurl_gw=s4=newurl_gw
+            else:
+                s3=s4='0'
+
+            newurl_qmo=scraper('post_url','qmoleenglish',0)
+            if newurl_qmo!=oldurl_qmo:
+                s5=("".join('\n'.join(scraper('text','qmoleenglish',0)).split('#')[0])).replace('\n\n','$').replace('\n','').replace('$','\n')
+                s7='\n'.join(scraper('images','qmoleenglish',0))
+                oldurl_qmo=s6=newurl_qmo
+            else:
+                s5=s6=s7='0'
+
+            i+=1
+
+            if s1!=s2 and s3!=s4:
+                s=s1+'\n'+s2+'\n'+"------"+'\n'+s3+'\n'+s4
+            elif s1!=s2:
+                s=s1+'\n'+s2
+            elif s3!=s4:
+                s=s3+'\n'+s4
+            else:
+                s=0
+            if s!=0 or s5!='0':
+                j+=1
+                logging.info(j)
+                if s!=0:
+                    await bot.get_channel(channel1).send(s+'\n')
+                if s5!='0':
+                    await bot.get_channel(channel1).send(s5+'\n')
+                    if s7!='0':
+                        await bot.get_channel(channel1).send(s7+'\n')
+                        await bot.get_channel(channel1).send(s6+'\n')
+                        await bot.get_channel(channel1).send(':warning:如果有些圖片沒有顯示，可以點擊貼文的URL'+'\n')
+                await bot.get_channel(channel1).send(str(j))
         except Exception:
-
-            await asyncio.sleep(300)
-            continue
-        #s135=text,s246=url,s7=images
-        if newurl_LEA!=oldurl_LEA:
-            s1=scraper('text','LearningEnglishAmericanWay',-1)
-            oldurl_LEA=s2=newurl_LEA
-        else:
-            s1=s2='0'
-
-        newurl_gw=scraper('post_url','gainwind',-1)
-        if newurl_gw!=oldurl_gw:
-            s3=scraper('text','gainwind',-1)
-            oldurl_gw=s4=newurl_gw
-        else:
-            s3=s4='0'
-
-        newurl_qmo=scraper('post_url','qmoleenglish',0)
-        if newurl_qmo!=oldurl_qmo:
-            s5=("".join('\n'.join(scraper('text','qmoleenglish',0)).split('#')[0])).replace('\n\n','$').replace('\n','').replace('$','\n')
-            s7='\n'.join(scraper('images','qmoleenglish',0))
-            oldurl_qmo=s6=newurl_qmo
-        else:
-            s5=s6=s7='0'
-
-        i+=1
-
-        if s1!=s2 and s3!=s4:
-            s=s1+'\n'+s2+'\n'+"------"+'\n'+s3+'\n'+s4
-        elif s1!=s2:
-            s=s1+'\n'+s2
-        elif s3!=s4:
-            s=s3+'\n'+s4
-        else:
-            s=0
-        if s!=0 or s5!='0':
-            j+=1
-            logging.info(j)
-            if s!=0:
-                await bot.get_channel(channel1).send(s+'\n')
-            if s5!='0':
-                await bot.get_channel(channel1).send(s5+'\n')
-                if s7!='0':
-                    await bot.get_channel(channel1).send(s7+'\n')
-                    await bot.get_channel(channel1).send(s6+'\n')
-                    await bot.get_channel(channel1).send(':warning:如果有些圖片沒有顯示，可以點擊貼文的URL'+'\n')
-            await bot.get_channel(channel1).send(str(j))       
+            await bot.get_channel(channel1).send(err)
+            pass
         await asyncio.sleep(300)
         
 @bot.command()
@@ -125,3 +123,6 @@ async def gsaterr(ctx,err):
                 await ctx.send("Already happened.")
             else:
                 await ctx.send(f"Time remaining: **{remaining} days**")
+bot.loop.create_task(scrape())
+logging.basicConfig(level=logging.INFO)
+bot.run(token)
