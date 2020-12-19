31117cc160bea34ba9cbaa620ea6496bb7d0b890
flyingcatt3 <jones184189@gmail.com>
2020-12-19 22:32:38 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from facebook_scraper import get_posts
 def scraper(sort,target):
-    s=[post[sort] for post in get_posts(target, pages=1)][-1]
+    return [post[sort] for post in get_posts(target, pages=1)][-1]
     
 scraper('post_url','gainwind')
 scraper('text','gainwind')
