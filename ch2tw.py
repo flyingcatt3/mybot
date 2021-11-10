85006b63dc390d1a36a2ef8dbb2276ae052608ed
flyingcatt3 <jones184189@gmail.com>
2021-11-11 01:20:39 +0800

    20211111

--- ch2tw.py
+++
@@ -1,6 +0,0 @@
-from opencc import OpenCC
-
-cc = OpenCC('s2twp')
-text = '他对女人的厌恶近乎神经质'
-
-print(cc.convert(text))
--- test.py
+++
@@ -1,7 +0,0 @@
-
-from facebook_scraper import get_posts
-def scraper(sort,target):
-    return [post[sort] for post in get_posts(target, pages=1)][-1]
-
-print(scraper('images','LearningEnglishAmericanWay'))
-
