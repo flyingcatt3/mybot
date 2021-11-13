35f7be26f452c19aaa12b0d600a619d2580308c9
flyingcatt3 <jones184189@gmail.com>
2021-11-13 11:19:53 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 def main():
     return "Your bot is alive!"
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=1113)
 def keep_alive():
     server = Thread(target=run)
     server.start()
