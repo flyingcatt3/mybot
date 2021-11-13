16f47b4349011f32cd6fe2ae2007e596983887b2
flyingcatt3 <jones184189@gmail.com>
2021-11-13 09:52:34 +0800

    20211113

--- main.py
+++ main.py
@@ -2,6 +2,17 @@
 import asyncio,logging,discord,datetime,random,os,keep_alive,itertools
 from discord.ext import commands,tasks
 
+def get_or_create_eventloop():
+    try:
+        return asyncio.get_event_loop()
+    except RuntimeError as ex:
+        if "There is no current event loop in thread" in str(ex):
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            return asyncio.get_event_loop()
+
+get_or_create_eventloop()
+
 #bot
 bot=commands.Bot(command_prefix='.')
 botid=782305505842036806
